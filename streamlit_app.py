bdde63a1a3e185c32a3b422753ec06dc79aadc19
hoad-oan <91173657+hoad-oan@users.noreply.github.com>
2026-02-01 17:08:11 -0500

    Update streamlit_app.py
    update nutritional value to link with fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,18 @@ ingredient_list = st.multiselect(
     max_selections = 6
 )
 
+# convert snowpark df to pandas df to use LOC function
+pd_df = my_dataframe.to_pandas()
+
 if ingredient_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'=fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutritional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
